dab3b40d507531474b9cae0a0f732d339b332b3a
pthomas0309 <preston.thomas355@gmail.com>
2021-06-22 15:16:40 -0500

    add authorization

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
+
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
